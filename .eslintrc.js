0e41dd611f9116ce70ba8a88ef2af8d4b4fc1381
wang.haoyu <228233464@qq.com>
2021-04-26 18:08:53 +0800

    feat: add eslint,add params

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  globals: {
+    ParamsException: true,
+    HttpException: true,
+    validateError: true
+  },
+  rules: {},
+};
--- validator/index.js
+++ validator/index.js
@@ -1,13 +1,17 @@
 const { ValidationError } = require('joi');
-
+const defaultsDeep = require('lodash/defaultsDeep');
 async function validate(ctx, schema) {
   try {
     const data = ctx.request.body;
-    await schema.validateAsync(data);
+    const query = ctx.request.query;
+    const params = ctx.request.params;
+    const request = defaultsDeep(data, query, params);
+    await schema.validateAsync(request);
   } catch (e) {
     if (e instanceof ValidationError) {
-      throw new ParamsException(e.message);
+      throw ParamsException(e.message);
     }
+    throw e;
   }
 }
 
